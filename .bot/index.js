db19a9b4f77b6c996e1f46abe7deba13291476f8
astrov-ost <110915581+astrov-ost@users.noreply.github.com>
2022-08-26 00:19:20 +0400

    FIX

--- .bot/index.js
+++ .bot/index.js
@@ -1,50 +1,31 @@
-const Discord = require('discord.js'); 
- const Token = process.env['TOKEN']; 
- const { Client, Intents } = require('discord.js'); 
- const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]}); 
- const keepAlive = require('./server.js'); 
+const express = require("express")
+const app = express()
+const prefix = "!"
+const Discord = require("discord.js")
+const client = new Discord.Client({
+intents: ["GUILDS", "GUILD_MESSAGES"]
+})
 
- const prefix = '!'; 
-  
- client.once('ready', async () => { 
-     client.user.setActivity('!help', {type: 'PLAYING'}); 
-     console.log('Ready!'); 
- }); 
-  
- client.on('message', (message) => { 
-     if(message.author.id === client.user.id) { return; } 
-     if(message.author.bot) return; 
- }); 
-  
- client.on('clickButton', async (button) => { 
-    //commands go here 
-     if(message.content == prefix + 'hi') { 
-         message.channel.send('Hello!'); 
-     } 
-     if(message.content == prefix + 'ping') { 
-         message.channel.send('Pong!'); 
-     } 
- }); 
-  
- client.on('messageUpdate', (oldMessage, newMessage) => { 
-      
- }); 
-  
- client.on('messageDelete', (message) => { 
-      
- });  
+app.get("/", (req, res) => {
+ res.send("Проект работает")
+})
 
-let mes1;
-mes1 = '1009374746049265766,
-1009377890892251256,
-1009512163506925638';
+app.listen(3000, () => {
+ console.log("Бот запущен!")
+})
 
-alert(mes1);
+client.on("messageCreate", message => {
+if(message.content === `${prefix}привет`){
+message.channel.send("привет, ${message.author.username}!")
+}
+if(message.content === `${prefix}ембед`) {
+let embed1 = new Discord.MessageEmbed()
+.setTitle("титл")
+.setDescription("дескрипшн")
+}
+})
 
-bot.on('guildMemberAdd', member => {
-    if(member in mes1)
-    {member.ban();} 
-});
-  
- keepAlive(); 
- client.login(Token);
+client.login(process.env.token) 
+
+// to Shell: npm i express
+// npm i discord.js@13.9
